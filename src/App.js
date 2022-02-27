f0003d09b3617b4b3227370b9313a3877ff1f7c9
Kahlil Manoj <55538246+kahlilmanoj@users.noreply.github.com>
2022-02-27 12:07:35 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -207,7 +207,7 @@ function App() {
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/elephant.png"} />
+            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
@@ -401,7 +401,7 @@ function App() {
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg
               alt={"example"}
-              src={"/config/images/elephant.png"}
+              src={"/config/images/example.gif"}
               style={{ transform: "scaleX(-1)" }}
             />
           </s.Container>
